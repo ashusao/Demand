1c17ead9abbc7c2a9c1e1b879948d0fe3000cec2
Ashu <ashusao@gmail.com>
2020-10-20 09:00:38 +0200

    Train diabled - eval enabled

--- main.py
+++ main.py
@@ -68,12 +68,12 @@ if __name__ == '__main__':
     print(X_train.shape, Y_train.shape, X_test.shape, Y_test.shape)
 
     X_train_one_hot, Y_train_one_hot = data_obj.load_one_hot_train(X_train, Y_train, X_test, Y_test, train=True)
-    train(config, X_train_one_hot, Y_train_one_hot, Y_train)
+    #train(config, X_train_one_hot, Y_train_one_hot, Y_train)
 
-    #X_test_one_hot, Y_test_one_hot = data_obj.load_one_hot_train(X_train, Y_train, X_test, Y_test, train=False)
-    #f1, bal_acc = evaluate(config, X_test_one_hot, Y_test_one_hot, Y_test)
+    X_test_one_hot, Y_test_one_hot = data_obj.load_one_hot_train(X_train, Y_train, X_test, Y_test, train=False)
+    f1, bal_acc = evaluate(config, X_test_one_hot, Y_test_one_hot, Y_test)
 
-    #log_result(config, X_train.shape[0], X_test.shape[0], bal_acc, f1)
+    log_result(config, X_train.shape[0], X_test.shape[0], bal_acc, f1)
 
 
 
