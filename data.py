24e80aee97417e2c1267535eeb0bfd70f0d029a3
Ashu <ashusao@gmail.com>
2020-12-23 16:53:18 +0100

    feature concat with dummies

--- data.py
+++ data.py
@@ -82,7 +82,7 @@ class Data:
         dum = pd.get_dummies(station_df,
                              prefix=['identifier', 'anschluss', 'type', 'suitable', 'zugang', 'cost', 'payment'],
                              columns=['identifier', 'anschluss', 'type', 'suitable_for', 'zugang', 'cost', 'payment'])
-        #feature_df = pd.concat([feature_df, dum], axis=1)
+        feature_df = pd.concat([feature_df, dum], axis=1)
         feature_df.power = feature_df['power'].map(lambda x: str(x)[:-1])
         feature_df.current = feature_df['current'].map(lambda x: str(x)[:-1])
 
