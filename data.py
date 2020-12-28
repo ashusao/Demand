95218e96e493eb5bbe6ed2a6057065e2105adbbb
Ashu <ashusao@gmail.com>
2020-12-28 10:50:24 +0100

    id_power

--- data.py
+++ data.py
@@ -91,22 +91,22 @@ class Data:
 
         #feature_df = dum
         feature_df = pd.concat([feature_df, dum], axis=1)
-        #feature_df.power = feature_df['power'].map(lambda x: str(x)[:-1])
+        feature_df.power = feature_df['power'].map(lambda x: str(x)[:-1])
         #feature_df.current = feature_df['current'].map(lambda x: str(x)[:-1])
 
         # drop unnecessary columns
         feature_df.drop(['lat', 'lon', 'provider', 'electricity', 'opening_hours',
                          'type', 'suitable_for', 'zugang', 'cost', 'payment',
-                         'anschlusse', 'power', 'current'], axis=1, inplace=True)
+                         'anschlusse', 'park_area', 'current'], axis=1, inplace=True)
 
-        #feature_df.power = feature_df.power.astype('int64')
+        feature_df.power = feature_df.power.astype('int64')
         #feature_df.current = feature_df.current.astype('int64')
         #feature_df.anschlusse = feature_df.anschlusse.astype('int64')
-        feature_df.park_area = feature_df.park_area.astype('float64')
+        #feature_df.park_area = feature_df.park_area.astype('float64')
 
         scaler = MinMaxScaler()
-        feature_df[['park_area']] = \
-            scaler.fit_transform(feature_df[['park_area']])
+        feature_df[['power']] = \
+            scaler.fit_transform(feature_df[['power']])
 
         '''titles = list(feature_df.columns)
         titles[1], titles[2] = titles[2], titles[1]
