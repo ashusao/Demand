11443c912cdede65366621a749aba1749c1298ad
Ashu <ashusao@gmail.com>
2020-12-28 12:28:13 +0100

    id_zugang

--- data.py
+++ data.py
@@ -84,10 +84,10 @@ class Data:
                              prefix=['identifier', 'anschluss', 'type', 'suitable', 'zugang', 'cost', 'payment'],
                              columns=['identifier', 'anschluss', 'type', 'suitable_for', 'zugang', 'cost', 'payment'])'''
 
-        feature_df = station_df[['identifier', 'suitable_for']]
+        feature_df = station_df[['identifier', 'zugang']]
         dum = pd.get_dummies(station_df,
-                             prefix=['identifier', 'suitable_for'],
-                             columns=['identifier', 'suitable_for'])
+                             prefix=['identifier', 'zugang'],
+                             columns=['identifier', 'zugang'])
 
         #feature_df = dum
         feature_df = pd.concat([feature_df, dum], axis=1)
