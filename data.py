4a4862f36a959127c49cae7c93e074d50e35fbc3
Ashu <ashusao@gmail.com>
2020-12-30 14:10:39 +0100

    all_poi

--- data.py
+++ data.py
@@ -107,8 +107,7 @@ class Data:
 
         # drop unnecessary columns
         feature_df.drop(['lat', 'lon', 'provider', 'electricity', 'opening_hours',
-                         'type', 'suitable_for', 'zugang', 'cost', 'payment',
-                         'restaurant', 'cafe', 'fast_food', 'pub'], axis=1, inplace=True)
+                         'type', 'suitable_for', 'zugang', 'cost', 'payment'], axis=1, inplace=True)
 
         feature_df.power = feature_df.power.astype('int64')
         feature_df.current = feature_df.current.astype('int64')
