e469a82ecd4d0903976bc05f7f3af7b85b875eeb
Ashu <ashusao@gmail.com>
2021-01-04 11:13:11 +0100

    industrial comm retail

--- data.py
+++ data.py
@@ -111,15 +111,17 @@ class Data:
         feature_df.drop(['lat', 'lon', 'provider', 'electricity', 'opening_hours',
                          'type', 'suitable_for', 'zugang', 'cost', 'payment',
                          'restaurant', 'cafe', 'fast_food', 'toilet', 'pub',
-                         'beach', 'river', 'residential', 'commercial', 'retail'], axis=1, inplace=True)
+                         'beach', 'river', 'residential'], axis=1, inplace=True)
 
         feature_df.power = feature_df.power.astype('int64')
         feature_df.current = feature_df.current.astype('int64')
         feature_df.anschlusse = feature_df.anschlusse.astype('int64')
 
         scaler = MinMaxScaler()
-        feature_df[['anschlusse', 'power', 'current', 'park_area', 'railway', 'airport', 'sea', 'industrial']] = \
-            scaler.fit_transform(feature_df[['anschlusse', 'power', 'current', 'park_area', 'railway', 'airport', 'sea', 'industrial']])
+        feature_df[['anschlusse', 'power', 'current', 'park_area', 'railway', 'airport', 'sea',
+                    'commercial', 'retail', 'industrial']] = \
+            scaler.fit_transform(feature_df[['anschlusse', 'power', 'current', 'park_area', 'railway', 'airport', 'sea',
+                                             'commercial', 'retail', 'industrial']])
 
         feature_df['airport'] = 1 - feature_df['airport']
         feature_df['railway'] = 1 - feature_df['railway']
