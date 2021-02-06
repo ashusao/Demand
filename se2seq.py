3c097e21526d5dc5b8fa0bda8983d3168ee4d8c5
Ashu <ashusao@gmail.com>
2021-02-06 15:09:57 +0100

    feature init hidden

--- se2seq.py
+++ se2seq.py
@@ -58,7 +58,7 @@ class Decoder(nn.Module):
         self.num_layers = num_layers
         self.gru = nn.GRU(input_size=input_size, hidden_size=hidden_size, num_layers=num_layers, batch_first=True)
         self.dropout = nn.Dropout(p=dropout)
-        self.linear = nn.Linear(2 * hidden_size, output_size)
+        self.linear = nn.Linear(hidden_size, output_size)
 
     def forward(self, input, hidden, features, decode):
         '''
@@ -72,8 +72,8 @@ class Decoder(nn.Module):
         output, hidden = self.gru(input.unsqueeze(1), hidden)
         output = self.dropout(output)
 
-        if decode == 'features':
-            output = torch.cat((output, features.unsqueeze(1)), 2) # concat decoder output and features
+        #if decode == 'features':
+        #    output = torch.cat((output, features.unsqueeze(1)), 2) # concat decoder output and features
 
         out = self.linear(output)
 
@@ -181,14 +181,14 @@ class Seq2Seq(nn.Module):
         num_layers = int(self.config['train']['num_layers'])
 
         hidden = self.encoder.init_hidden(batch_size).to(device)
-        encoder_out, hidden = self.encoder(source, hidden)
+        #encoder_out, hidden = self.encoder(source, hidden)
 
         if feat and decode == 'features':
             #intial hidden as features
             features = self.embedding(features)  # features  =====>>> hidden
-            #features = features.unsqueeze(0)  # add extra dimensino for num_layers
-            #features = features.repeat(num_layers, 1, 1)
-            #hidden[:, :, :features.shape[2]] = features  # fill intial hidden with avail features
+            features = features.unsqueeze(0)  # add extra dimensino for num_layers
+            features = features.repeat(num_layers, 1, 1)
+            hidden[:, :, :features.shape[2]] = features  # fill intial hidden with avail features
 
             #features = features.unsqueeze(0)  # add extra dimensino for num_layers
             #features = features.repeat(hidden.shape[0], 1, 1)  # copy features to each layers (num_layers, batch, hidden_size)
@@ -201,7 +201,7 @@ class Seq2Seq(nn.Module):
         #else:
         #    hidden = self.encoder.init_hidden(batch_size).to(device)
 
-
+        encoder_out, hidden = self.encoder(source, hidden)
         #hidden = self.encoder.init_hidden(batch_size).to(device)
         #print(hidden.shape)
 
